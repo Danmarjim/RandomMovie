67c657b569658d5138a2e61681d75473f865d765
Daniel Martin Jimenez <daniel.martin@ubeeqo.com>
2020-09-25 20:55:12 +0200

    Quick refactor Keep it simple

--- RandomMovie/RandomMovie/Design/Components/Colors/Mode/Dark/DarkMode.swift
+++ RandomMovie/RandomMovie/Design/Components/Colors/Mode/Dark/DarkMode.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 struct DarkMode: Mode {
-  let main: Variation = DarkVariation()
-  let textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+  let background = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+  let text = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
   public init() {}
 }
--- RandomMovie/RandomMovie/Design/Components/Colors/Mode/Light/Variation/LightVariation.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-struct LightVariation: Variation {
-  let backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-}
